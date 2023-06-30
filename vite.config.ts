3e9d15a4d2c14f6059e540146fac7a3a1c5e9141
Subliminal Guy <subliminal_kid@posteo.de>
2023-06-30 16:46:32 +0200

    Change Proxy Address

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: { "/api": "https://helmut-crud.subliminalguy.repl.co/" },
+    proxy: { "/api": "https://helmutcrud.codebabbler.org" },
   },
 });
