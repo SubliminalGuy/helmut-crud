76001f2a861eb15bc02dbddb174067b4a15a924f
Subliminal Guy <subliminal_kid@posteo.de>
2023-07-03 16:23:38 +0200

    Add MariaDB connection

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,20 @@
 import { remultExpress } from "remult/remult-express";
+import { createKnexDataProvider } from "remult/remult-knex";
 import { Project } from "../shared/Project";
 import { Hostname } from "../shared/Hostname";
 
-export const api = remultExpress({ entities: [Project, Hostname] });
+require("dotenv").config();
+
+export const api = remultExpress({
+  entities: [Project, Hostname],
+  dataProvider: createKnexDataProvider({
+    client: "mysql2",
+    connection: {
+      host: "localhost",
+      port: 3306,
+      database: "test",
+      user: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    },
+  }),
+});
