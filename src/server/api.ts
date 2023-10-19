ff03e12a7d4985c20a3f90161601e842151951f2
Subliminal Guy <subliminal_kid@posteo.de>
2023-10-19 14:30:09 +0200

    🔧 fix(api.ts): Füge die Entität ProTools zur Liste der Entitäten hinzu 📝 Funktion(ProTools.ts): Füge die Entität ProTools hinzu, um ProTools-Daten zu speichern

--- src/server/api.ts
+++ src/server/api.ts
@@ -3,11 +3,12 @@ import { createKnexDataProvider } from "remult/remult-knex";
 import { Project } from "../shared/Project";
 import { Hostname } from "../shared/Hostname";
 import { AiMetadata } from "../shared/AiMetadata";
+import { ProTools } from "../shared/ProTools";
 
 require("dotenv").config();
 
 export const api = remultExpress({
-  entities: [Project, Hostname, AiMetadata],
+  entities: [Project, Hostname, AiMetadata, ProTools],
   dataProvider: createKnexDataProvider({
     client: "mysql2",
     connection: {
