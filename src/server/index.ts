1cbf62faf3ec24edf71133a3bfd6dc60b9078025
david.schwertgen <david.schwertgen@rbb-online.de>
2023-10-27 16:48:23 +0200

    Finish Helmut CheckIn Route

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import { api } from "./api";
 import { getAudioFiles } from "./fileService";
 import { remult } from "remult"
 import { ProTools } from "../shared/ProTools";
+import dummyjson from "../../dummy-helmut-body.json"
 
 const app = express();
 app.use(api);
@@ -21,7 +22,40 @@ app.post("/api/checkin", api.withRemult, async (req, res) => {
   const proTools = remult.repo(ProTools)
   try {
     const data = await proTools.find({where: {projectname: req.body.projectData.projectName}})
-    const projektId = data[0].projectId
+    
+    const projektData = data[0]
+    const payload = { ...dummyjson };
+    const newMetadata = [];
+    for (const object of payload.metadata) {
+        switch (object.name) {
+            case 'PT Projectname':
+                object.value = projektData.projectname;
+                break;
+            case 'PT ProjectID':
+                object.value = projektData.projectId;
+                break;
+            case 'PT JobID':
+                object.value = projektData.jobId;
+                break;
+            case 'PT MXF':
+                object.value = projektData.mxfPath;
+                break;
+            case 'PT Autor':
+                object.value = projektData.autor;
+                break;
+            case 'PT AudioST':
+                object.value = req.body.projectData.audioSpur1;
+                break;
+            case 'PT AudioIT':
+                // condition to handle possibility of empty audio track
+                object.value = (req.body.projectData.audioSpur2 === '') ? '' : req.body.projectData.audioSpur2;
+                break;
+        }
+        newMetadata.push(object);
+      }
+      payload.metadata = newMetadata;
+      console.log(payload.metadata)
+        const projektId = projektData.projectId
     res.json({message: projektId, success: true})
   }
   catch {
